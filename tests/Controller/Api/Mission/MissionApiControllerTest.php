564febf113caef7f58b348ac830ac5a206aba295
ghriim <vignierquentin@yahoo.fr>
2018-09-04 10:30:07 +0200

    Add test on MissionApi

---
+++ tests/Controller/Api/Mission/MissionApiControllerTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Tests\Controller\Api\Mission;
+
+use App\Tests\Controller\Api\AbstractBaseApiTest;
+use Symfony\Component\HttpFoundation\Response;
+
+class MissionApiControllerTest extends AbstractBaseApiTest
+{
+    public function testGetManyUnauthorized(): void
+    {
+        $client = static::createClient();
+        $this->buildGetRequest($client, 'missions');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testGetManyAuthorized(): void
+    {
+        $client = $this->buildAuthenticatedAdmin();
+        $this->buildGetRequest($client, 'missions');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertEquals(
+            $this->loadDataFromJsonFile('json/missions'),
+            json_decode($response->getContent(), true)
+        );
+    }
+
+    public function testGetOneUnauthorized(): void
+    {
+        $client = static::createClient();
+        $this->buildGetRequest($client, 'missions/0000');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testGetOneAuthorized(): void
+    {
+        $client = $this->buildAuthenticatedUser();
+        $this->buildGetRequest($client, 'missions/0000');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_NOT_IMPLEMENTED, $response->getStatusCode());
+        $this->assertEmpty($response->getContent());
+    }
+
+    public function testPostUnauthorized(): void
+    {
+        $client = static::createClient();
+        $this->buildPostRequest($client, 'missions');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testPostAuthorized(): void
+    {
+        $client = $this->buildAuthenticatedUser();
+        $this->buildPostRequest($client, 'missions');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_NOT_IMPLEMENTED, $response->getStatusCode());
+        $this->assertEmpty($response->getContent());
+    }
+
+    public function testPutUnauthorized(): void
+    {
+        $client = static::createClient();
+        $this->buildPutRequest($client, 'missions/0000');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testPutAuthorized(): void
+    {
+        $client = $this->buildAuthenticatedUser();
+        $this->buildPutRequest($client, 'missions/0000');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_NOT_IMPLEMENTED, $response->getStatusCode());
+        $this->assertEmpty($response->getContent());
+    }
+
+    public function testDeleteUnauthorized(): void
+    {
+        $client = static::createClient();
+        $this->buildDeleteRequest($client, 'missions/0000');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testDeleteAuthorized(): void
+    {
+        $client = $this->buildAuthenticatedUser();
+        $this->buildDeleteRequest($client, 'missions/0000');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_NOT_IMPLEMENTED, $response->getStatusCode());
+        $this->assertEmpty($response->getContent());
+    }
+}
