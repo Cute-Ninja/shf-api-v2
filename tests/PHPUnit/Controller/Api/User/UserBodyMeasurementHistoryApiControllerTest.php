50ba7344d96e0943a4de4ce6e33d83773eeef903
ghriim <vignierquentin@yahoo.fr>
2018-10-24 13:39:49 +0200

    Add tests on get body measurement history of a given user

---
+++ tests/PHPUnit/Controller/Api/User/UserBodyMeasurementHistoryApiControllerTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Tests\PHPUnit\PHPUnit\Controller\Api\User;
+
+use App\Tests\PHPUnit\Controller\Api\AbstractBaseApiTest;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @covers \App\Controller\Api\User\UserBodyMeasurementHistoryApiController
+ */
+class UserBodyMeasurementHistoryApiControllerTest extends AbstractBaseApiTest
+{
+    public function testGetManyUnauthorized(): void
+    {
+        $client = static::createClient();
+        $this->buildGetRequest($client, 'users/ghriim/body-measurements/history');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testGetManyAuthorized(): void
+    {
+        $client = $this->buildAuthenticatedUser();
+        $this->buildGetRequest($client, 'users/ghriim/body-measurements/history');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertEquals(
+            $this->loadDataFromJsonFile('json/body_measurement_history_ghriim'),
+            json_decode($response->getContent(), true)
+        );
+    }
+
+    public function testGetOneUnauthorized(): void
+    {
+        $client = static::createClient();
+        $this->buildGetRequest($client, 'users/ghriim/body-measurements/history/1');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testGetOneAuthorized(): void
+    {
+        $client = $this->buildAuthenticatedUser();
+        $this->buildGetRequest($client, 'users/ghriim/body-measurements/history/1');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_NOT_IMPLEMENTED, $response->getStatusCode());
+        $this->assertEmpty($response->getContent());
+    }
+
+    public function testPostUnauthorized(): void
+    {
+        $client = static::createClient();
+        $this->buildPostRequest($client, 'users/ghriim/body-measurements/history');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testPostAuthorized(): void
+    {
+        $client = $this->buildAuthenticatedUser();
+        $this->buildPostRequest($client, 'users/ghriim/body-measurements/history');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_NOT_IMPLEMENTED, $response->getStatusCode());
+        $this->assertEmpty($response->getContent());
+    }
+
+    public function testPutUnauthorized(): void
+    {
+        $client = static::createClient();
+        $this->buildPutRequest($client, 'users/ghriim/body-measurements/history/1');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testPutAuthorized(): void
+    {
+        $client = $this->buildAuthenticatedUser();
+        $this->buildPutRequest($client, 'users/ghriim/body-measurements/history/1');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_NOT_IMPLEMENTED, $response->getStatusCode());
+        $this->assertEmpty($response->getContent());
+    }
+
+    public function testDeleteUnauthorized(): void
+    {
+        $client = static::createClient();
+        $this->buildDeleteRequest($client, 'users/ghriim/body-measurements/history/1');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testDeleteAuthorized(): void
+    {
+        $client = $this->buildAuthenticatedUser();
+        $this->buildDeleteRequest($client, 'users/ghriim/body-measurements/history/1');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_NOT_IMPLEMENTED, $response->getStatusCode());
+        $this->assertEmpty($response->getContent());
+    }
+}
